adeeb2cef0002045aba358a9128ab1e8052f8097
Aniket Kumar <aniketshraff@gmail.com>
2026-05-11 11:38:28 +0530

    Feet class implemented with double value and overide euals method

---
+++ BusinessLogic/Feet.cs
@@ -0,0 +1,28 @@
+﻿namespace QMAPP.BusinessLogic
+{
+    public class Feet
+    {
+        internal double FeetValue { get; set; }
+
+        public Feet(double FeetValue)
+        {
+            this.FeetValue = FeetValue;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            // null check
+            if (obj == null)
+                return false;
+
+            // type check
+            if (obj.GetType() != this.GetType())
+                return false;
+
+            // type cast
+            Feet other = (Feet) obj;
+            // check 
+            return this.FeetValue== other.FeetValue;
+        }
+    }
+}
