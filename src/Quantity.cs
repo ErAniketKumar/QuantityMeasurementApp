5df76aae996e4e20c691d817aa3131e6d9cc36df
ErAniketKumar <aniketshraff@gmail.com>
2026-06-03 12:00:15 +0530

    UC 10 Make generic class for remove the reducency code

---
+++ src/Quantity.cs
@@ -0,0 +1,93 @@
+namespace QMAPP.src;
+public class Quantity<U> where U : IMeasurable
+{
+    private readonly double _value;
+    private readonly U _unit;
+
+    public Quantity(double value, U unit)
+    {
+        _value = value;
+        _unit = unit;
+    }
+
+    public double Value => _value;
+
+    public U Unit => _unit;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Quantity<U> other)
+            return false;
+
+        double firstBase =
+            _unit.ConvertToBaseUnit(_value);
+
+        double secondBase =
+            other._unit.ConvertToBaseUnit(other._value);
+
+        return Math.Abs(firstBase - secondBase)
+               < 0.0001;
+    }
+
+    public Quantity<U> ConvertTo(U targetUnit)
+    {
+        double baseValue =
+            _unit.ConvertToBaseUnit(_value);
+
+        double converted =
+            targetUnit.ConvertFromBaseUnit(baseValue);
+
+        return new Quantity<U>(
+            converted,
+            targetUnit
+        );
+    }
+
+    public Quantity<U> Add(Quantity<U> other)
+    {
+        double firstBase =
+            _unit.ConvertToBaseUnit(_value);
+
+        double secondBase =
+            other._unit.ConvertToBaseUnit(other._value);
+
+        double sumBase =
+            firstBase + secondBase;
+
+        double result =
+            _unit.ConvertFromBaseUnit(sumBase);
+
+        return new Quantity<U>(
+            result,
+            _unit
+        );
+    }
+
+    public Quantity<U> Add(
+        Quantity<U> other,
+        U targetUnit
+    )
+    {
+        double firstBase =
+            _unit.ConvertToBaseUnit(_value);
+
+        double secondBase =
+            other._unit.ConvertToBaseUnit(other._value);
+
+        double sumBase =
+            firstBase + secondBase;
+
+        double result =
+            targetUnit.ConvertFromBaseUnit(sumBase);
+
+        return new Quantity<U>(
+            result,
+            targetUnit
+        );
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_value, _unit);
+    }
+}
