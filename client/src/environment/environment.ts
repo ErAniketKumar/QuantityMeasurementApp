346c5c5ff8f14de85a87eb189414740540729ec5
ErAniketKumar <aniketshraff@gmail.com>
2026-06-20 17:35:29 +0530

    prod backend confg updqate

--- client/src/environment/environment.ts
+++ client/src/environment/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5098/api',
+  apiUrl:
+    'https://quantitymeasurment-gvgjdfewgdfpeqea.centralindia-01.azurewebsites.net/api',
   googleClientId:
     '129694712018-vi6ql1l3gl2ml2us2d359m10500hnbi7.apps.googleusercontent.com',
 };
